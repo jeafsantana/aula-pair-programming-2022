341311f1e555bab3d17276067e2650734544e7fd
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2022-05-20 21:25:27 -0300

    Criando classe customizada para a celula de pessoa e ligando componentes.

---
+++ agenda/agenda/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  agenda
+//
+//  Created by Adriano Souza Costa on 20/05/22.
+//
+
+import UIKit
+
+class PersonCell: UITableViewCell {
+
+    @IBOutlet weak var personName: UILabel!
+
+}
--- agenda/agenda/ViewController.swift
+++ agenda/agenda/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pesquisaTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func pesquisar() {}
 
 }
 
