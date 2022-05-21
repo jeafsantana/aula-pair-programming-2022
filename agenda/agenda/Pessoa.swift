fded163d4ec64786cd93bb9b9c119aec73f2f7a5
Juliane Lopes Moreira Rodrigues <juliane.lopes30@yahoo.com.br>
2022-05-20 22:25:20 -0300

    Criar class pessoa

---
+++ agenda/agenda/Pessoa.swift
@@ -0,0 +1,26 @@
+//
+//  Pessoa.swift
+//  agenda
+//
+//  Created by Juliane Lopes Moreira Rodrigues on 20/05/22.
+//
+
+import Foundation
+
+class Pessoa {
+    let nome: String
+    let idade: Int
+    let endereco: String
+    let telefone: String
+    let email: String
+    let foto: String
+    
+    init(nome: String, idade:Int, endereco: String, telefone: String, email: String, foto: String){
+        self.nome = nome
+        self.idade = idade
+        self.endereco = endereco
+        self.telefone = telefone
+        self.email = email
+        self.foto = foto
+    }
+}
