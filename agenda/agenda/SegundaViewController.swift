acd83aee684687e3c41cfbec3ffba83f9a7381ac
Marcelo Falcão <marcelo_falcaoc@hotmail.com>
2022-05-23 19:44:12 -0300

    Add nova tela e funcao para ajuste

---
+++ agenda/agenda/SegundaViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SegundaViewController.swift
+//  agenda
+//
+//  Created by Marcelo Falcao Costa Filho on 23/05/22.
+//
+
+import UIKit
+
+class SegundaViewController: UIViewController {
+
+    @IBOutlet weak var nomeLabel: UILabel!
+    @IBOutlet weak var idadeLabel: UILabel!
+    @IBOutlet weak var enderecoLabel: UILabel!
+    @IBOutlet weak var telefoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    func configuraDados(pessoa: Pessoa) {
+        nomeLabel.text = pessoa.nome
+        idadeLabel.text = String(pessoa.idade)
+        enderecoLabel.text = pessoa.endereco
+        telefoneLabel.text = pessoa.telefone
+        emailLabel.text = pessoa.email
+        posterImageView.image = UIImage.init(named: pessoa.foto)
+    }
+}
